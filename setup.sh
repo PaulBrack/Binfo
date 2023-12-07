9845e6c820b36eae1938342a2a40eed03c0ae3d7
Paul Brack <paulbrack48@gmail.com>
2023-12-07 13:25:03 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt install pup
+sudo apt install git
+sudo apt install pigpio
+git clone https://github.com/7402/blinkt.git
+cd blinkt
+make
+sudo make install
+sudo systemctl enable pigpiod
+cd ..
+sudo chmod +x binfo.sh
+echo -e "0 0 * * * ~/binfo.sh\n@reboot ~/binfo.sh" | crontab -
