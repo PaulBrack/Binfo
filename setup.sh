5b16a8dc89c43dac5f85f61268d03249418fa162
Paul Brack <paulbrack48@gmail.com>
2023-12-07 18:10:37 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 sudo apt install pup
 sudo apt install git
-sudo apt install pigpio
-git clone https://github.com/7402/blinkt.git
-cd blinkt
-make
-sudo make install
-sudo systemctl enable pigpiod
+
+#set up the blinkt
+curl -sS https://get.pimoroni.com/blinkt | bash
+
+#set up the binfo script
 mv ~/Binfo/binfo.sh /usr/lib/binfo.sh
 sudo chmod +x /usr/lib/binfo.sh
 echo -e "0 0 * * * /usr/lib/binfo.sh\n@reboot /usr/lib/binfo.sh" | crontab -
 read -p "Please enter your UPRN " uprn
 echo $uprn > /usr/lib/uprn
 sudo /usr/lib/binfo.sh
+
+#set up the wifi AP
+cd ~
+sudo apt install python3
+git clone https://github.com/jasbur/RaspiWiFi.git
+sudo python3 ./RaspiWiFi/initial_setup.py
