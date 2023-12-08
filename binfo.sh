1e79eb32f2d889f2f24df77ab6865485dcf9133d
Paul Brack <paulbrack48@gmail.com>
2023-12-08 20:23:06 +0000

    Update binfo.sh

--- binfo.sh
+++ binfo.sh
@@ -1,13 +1,7 @@
-cd ~/Binfo
-
-#get the uprn
-uprn=$(cat uprn)
+!/bin/bash
 
 #Get the web page
 binfo=$(curl 'https://www.manchester.gov.uk/Checkyourbincollectiondays' --data-raw 'mcc_bin_dates_uprn=000077086062&mcc_bin_dates_submit=Go')
-
-#Today's date in same format as site
-today=$(date +'%e %b %Y' | xargs)
 
 #Get binfo
 bluebin=$(echo $binfo | pup 'div.collection h3:contains("Blue") + p text{}' | awk '{print $4, $5, $6}')
@@ -16,7 +10,8 @@ greenbin=$(echo $binfo | pup 'div.collection h3:contains("Green") + p text{}' | 
 blackbin=$(echo $binfo | pup 'div.collection h3:contains("Black") + p text{}' | awk '{print $4, $5, $6}')
 
 #Compare dates
-[[ "$bluebin" == "$today" ]] && blinkt 11000000 blue
-[[ "$brownbin" == "$today" ]] && blinkt 00110000 orange
-[[ "$greenbin" == "$today" ]] && blinkt 00001100 green
-[[ "$blackbin" == "$today" ]] && blinkt 00000011 white
+sudo blinkt black
+[[ $(( $(date -d "$bluebin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 11000000 blue
+[[ $(( $(date -d "$brownbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00110000 orange
+[[ $(( $(date -d "$greenbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00001100 green
+[[ $(( $(date -d "$blackbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00000011 white
