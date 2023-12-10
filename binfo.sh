286064aa135e2abcb77ce01a24c4a94314cf35e4
Paul Brack <paulbrack48@gmail.com>
2023-12-10 16:44:27 +0000

    Update binfo.sh

--- binfo.sh
+++ binfo.sh
@@ -1,4 +1,14 @@
-!/bin/bash
+#!/bin/bash
+
+#Set all LEDs red when no binfo
+sudo blinkt red
+
+#For startup job, retry every 10s for 10 minutes if network is down - it may not be up yet
+counter=0
+until [ $counter -ge 60 ] || ip addr show wlan0 | grep -qE 'inet ([[:digit:]]+\.?)+'; do
+   sleep 10
+   ((counter++))
+done
 
 #Get the web page
 binfo=$(curl 'https://www.manchester.gov.uk/Checkyourbincollectiondays' --data-raw 'mcc_bin_dates_uprn=000077086062&mcc_bin_dates_submit=Go')
@@ -9,9 +19,9 @@ brownbin=$(echo $binfo | pup 'div.collection h3:contains("Brown") + p text{}' | 
 greenbin=$(echo $binfo | pup 'div.collection h3:contains("Green") + p text{}' | awk '{print $4, $5, $6}')
 blackbin=$(echo $binfo | pup 'div.collection h3:contains("Black") + p text{}' | awk '{print $4, $5, $6}')
 
-#Compare dates
+#Compare dates - show on bin day and day before
 sudo blinkt black
-[[ $(( $(date -d "$bluebin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 11000000 blue
-[[ $(( $(date -d "$brownbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00110000 orange
-[[ $(( $(date -d "$greenbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00001100 green
-[[ $(( $(date -d "$blackbin" +"%s") - $(date +"%s") )) -lt $((7 * 24 * 60 * 60)) ]] && sudo blinkt 00000011 white
+[[ $(( $(date -d "$bluebin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 11000000 blue
+[[ $(( $(date -d "$brownbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00110000 orange
+[[ $(( $(date -d "$greenbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00001100 green
+[[ $(( $(date -d "$blackbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00000011 white
