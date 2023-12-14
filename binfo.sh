06592c94a5ba86740c9e8b2dab53f255cbd8c702
Paul Brack <paulbrack48@gmail.com>
2023-12-14 17:44:34 +0000

    Update binfo.sh

--- binfo.sh
+++ binfo.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-#Set all LEDs red when no binfo
-sudo blinkt red
-
-#For startup job, retry every 10s for 10 minutes if network is down - it may not be up yet
+#For startup job, retry every 1s for 10 minutes if network is down - it may not be up yet
 counter=0
-until [ $counter -ge 60 ] || ip addr show wlan0 | grep -qE 'inet ([[:digit:]]+\.?)+'; do
-   sleep 10
+until [ $counter -ge 600 ] || ip addr show wlan0 | grep -qE 'inet ([[:digit:]]+\.?)+'; do
+   sleep 1   
    ((counter++))
+   if [ $((foo%2)) -eq 0 ];
+   then
+      sudo blinkt blue
+   else
+      sudo blinkt black
+   fi
 done
+
+sudo blinkt green
 
 #Get the web page
 binfo=$(curl 'https://www.manchester.gov.uk/Checkyourbincollectiondays' --data-raw 'mcc_bin_dates_uprn=000077086062&mcc_bin_dates_submit=Go')
 
-#Get binfo
+# Function to check if a date is valid
+is_valid_date() {
+   date -d "$1" > /dev/null 2>&1
+}
+
+# Extracting Bin Collection Dates
 bluebin=$(echo $binfo | pup 'div.collection h3:contains("Blue") + p text{}' | awk '{print $4, $5, $6}')
 brownbin=$(echo $binfo | pup 'div.collection h3:contains("Brown") + p text{}' | awk '{print $4, $5, $6}')
 greenbin=$(echo $binfo | pup 'div.collection h3:contains("Green") + p text{}' | awk '{print $4, $5, $6}')
 blackbin=$(echo $binfo | pup 'div.collection h3:contains("Black") + p text{}' | awk '{print $4, $5, $6}')
 
-#Compare dates - show on bin day and day before
-sudo blinkt black
-[[ $(( $(date -d "$bluebin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 11000000 blue
-[[ $(( $(date -d "$brownbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00110000 orange
-[[ $(( $(date -d "$greenbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00001100 green
-[[ $(( $(date -d "$blackbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00000011 white
+# Check if all dates are valid
+if ! is_valid_date "$bluebin" || ! is_valid_date "$brownbin" || ! is_valid_date "$greenbin" || ! is_valid_date "$blackbin"; then
+   # Set Blinkt! to red if any date is invalid
+   sudo blinkt red
+else
+   # Comparing Dates and Blinking LEDs
+   sudo blinkt black
+   [[ $(( $(date -d "$bluebin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 11000000 blue
+   [[ $(( $(date -d "$brownbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00110000 orange
+   [[ $(( $(date -d "$greenbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00001100 green
+   [[ $(( $(date -d "$blackbin" +"%s") - $(date +"%s") )) -lt $((24 * 60 * 60)) ]] && sudo blinkt 00000011 white
+fi
